0939156aa0164586a5929d82a59698e027a0789b
Ayoub Zaki <ayoub.zaki@embetrix.com>
2025-12-09 10:28:46 +0100

    qstopwatch: add latest qt6 qml update

--- package/qstopwatch/qstopwatch.mk
+++ package/qstopwatch/qstopwatch.mk
@@ -1,4 +1,4 @@
-QSTOPWATCH_VERSION = de9573ec39c6bf51dd3d486d3b42058c01090c05
+QSTOPWATCH_VERSION = 105b01e533d6dc4698e65cfd47c5e95b4b0944e7
 QSTOPWATCH_SITE = https://github.com/embetrix/qstopwatch.git
 QSTOPWATCH_SITE_METHOD = git
 
@@ -7,10 +7,25 @@ QSTOPWATCH_LICENSE_FILES = LICENSE
 
 QSTOPWATCH_DEPENDENCIES = \
 	host-pkgconf \
-	qt5tools qt5base
+	host-qt6tools \
+	host-qt6shadertools \
+	qt6base \
+	qt6declarative \
+	qt6svg
 
 define QSTOPWATCH_INSTALL_TARGET_CMDS
 	cp $(@D)/qstopwatch $(TARGET_DIR)/usr/bin/qstopwatch
+endef
+
+define QSTOPWATCH_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(QSTOPWATCH_PKGDIR)/S43qstopwatch \
+		$(TARGET_DIR)/etc/init.d/S43qstopwatch
+endef
+
+define QSTOPWATCH_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -d $(TARGET_DIR)/etc/qstopwatch
+	$(INSTALL) -D -m 0644 $(QSTOPWATCH_PKGDIR)/qstopwatch.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/qstopwatch.service
 endef
 
 $(eval $(cmake-package))
